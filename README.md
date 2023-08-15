0def83a69d51b568d03551894f6889e23a794a2c
Geraldo Andrade <hi@geraldoandrade.com>
2023-08-15 20:48:56 +0200

    feat(core): setup syncthing

---
+++ README.md
@@ -0,0 +1,9 @@
+# Setup Home Server Elessar
+
+This is the script for set up my home server machine.
+
+
+# How to run?
+
+    $ sudo apt install curl
+    $ curl -sL https://raw.githubusercontent.com/geraldoandradee/elessar/main/setup.sh | sudo -E bash -    
---
+++ setup.sh
@@ -0,0 +1,33 @@
+#/usr/bin/env bash
+
+check_root() {
+  if [ $(whoami) != "root" ]
+  then
+    echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+    echo "######### This script must be executed as root! #########"
+    echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+    exit 1
+  fi
+}
+
+
+
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo "::::::::::::: Setup Script For $(lsb_release -d -s) ::::::::::::::"
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo ""
+echo ""
+echo "This setup is meant for setup home server"
+echo ""
+echo ""
+
+check_root
+
+apt update
+apt install ansible git-core -y
+
+cd
+git clone https://github.com/geraldoandradee/elessar.git
+cd setup-zorin-os-16
+ansible-playbook setup.yml -i hosts.ini
+rm -rf setup-zorin-os-16
